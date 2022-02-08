761d6db18274657fb8d53619415394d5307a50f2
dsoldera <daniellesoldera@gmail.com>
2022-02-08 20:56:02 -0300

    updates of the React course

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { Counter } from './components/Counter';
+import React from 'react';
 import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App() {
-    return <RepositoryList />
+    return (
+        <>
+            <RepositoryList />
+            <Counter />
+        </>
+    )
 }
