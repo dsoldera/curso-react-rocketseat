1c615343fb350ca324e20b7a70433bbb2feeed46
dsoldera <daniellesoldera@gmail.com>
2022-02-15 10:19:23 -0300

    create folder chapter one and finish the Basic React Course

--- chapter-I/src/App.tsx
+++ chapter-I/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
@@ -6,6 +5,6 @@ export function App() {
     return (
         <>
             <RepositoryList />
-         </>
+        </>
     )
 }
