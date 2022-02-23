fe6a9cf23cc09eac18df62ed138d1950adeace7b
dsoldera <dsoldera@gmail.com>
2022-02-22 22:15:45 -0300

    update studies 22-02-2022

--- chapter-II/dtmoney/src/components/Header/index.tsx
+++ chapter-II/dtmoney/src/components/Header/index.tsx
@@ -1,33 +1,17 @@
 import  logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
-import Modal from 'react-modal';
-import { useState } from 'react';
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
 
-export function Header () {
-    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
-
-    function handleOpenNewTransactionModal () {
-        setIsNewTransactionModalOpen(true);
-    }
-
-    function handleCloseNewTransactionModal () {
-        setIsNewTransactionModalOpen(false);
-    }
-    
+export function Header ({onOpenNewTransactionModal}:HeaderProps) {
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="DT Money"/>
-                <button type="button" onClick={handleOpenNewTransactionModal}>
+                <button type="button" onClick={onOpenNewTransactionModal}>
                     Nova Transação
                 </button>
-
-                <Modal 
-                    isOpen={isNewTransactionModalOpen} 
-                    ariaHideApp={false}
-                    onRequestClose={handleCloseNewTransactionModal}>
-                        <h2>Cadastrar Transaçao</h2>   
-                </Modal>
             </Content>
         </Container>
     )
--- chapter-II/dtmoney/src/components/NewTransactionModal/index.tsx
+++ chapter-II/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,73 @@
-import { Container } from "./styles";
+import Modal from 'react-modal';
+import { Container, TransactionTypeContainer, RadioBox } from './styles';
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { useState } from 'react';
 
-export function NewTransactionModal() {
+
+Modal.setAppElement('#root');
+interface NewTransactionModalProps {
+    isOpen: boolean;
+    onRequestClose: () => void;
+}
+
+export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModalProps) {
+    const [type, setType] = useState('deposit');
 
     return (
-        <Container>
-        </Container>
+        <Modal 
+            isOpen={isOpen} 
+            ariaHideApp={false}
+            onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content">
+
+        <button 
+          type="button" 
+          onClick={onRequestClose} 
+          className="react-modal-close">
+          <img src={closeImg} alt="Fechar modal"/>
+        </button>    
+            <Container>
+                <h2>Cadastrar Transaçao</h2>
+
+                <input 
+                    placeholder="Titulo"
+                />
+
+                <input 
+                    type="number"
+                    placeholder="Valor"
+                />
+
+                <input 
+                    placeholder="Categoria"
+                />
+                
+                <TransactionTypeContainer>
+                <RadioBox
+                    type="button"
+                    // className={type === 'deposit' ? 'active' : ''}
+                    onClick={() => {setType('deposit')}}
+                    isActive={type === 'deposit'}
+                    >
+                    <img src={incomeImg} alt="Entrada"/>
+                    <span>Entrada</span>
+                </RadioBox>
+                <RadioBox 
+                    type="button"
+                    onClick={() => {setType('withdraw')}}
+                    isActive={type === 'withdraw'}>
+                    <img src={outcomeImg} alt="Saída" />
+                    <span>Saída</span>
+                </RadioBox>
+                </TransactionTypeContainer>
+
+                <button type="submit">
+                    Cadastrar
+                </button>
+            </Container>
+      </Modal>
     );
 }
